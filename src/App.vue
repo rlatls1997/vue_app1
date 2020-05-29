e62e8fc2ca0fcf9575b401e186f40d1471e43ad2
Kim, Sinhyuk <rlatls1997@gmail.com>
2020-05-29 15:02:05 +0900

    20.05.29 commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld message="첫번째 Vue.js App 입니다" />
   </div>
 </template>
 
@@ -16,13 +15,7 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+div#app { padding: 30px; margin: 30px auto; max-width: 400px;
+      border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; }
 </style>
